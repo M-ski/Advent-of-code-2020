3f397f3e7f62a9d44c145d68f7f99ff8b3ab9916
M.ski <30794258+M-ski@users.noreply.github.com>
2021-01-02 18:23:23 +0000

    AOC-0 updating the home component

--- web/src/components/home/Home.tsx
+++ web/src/components/home/Home.tsx
@@ -7,8 +7,8 @@ export class Home extends Component<{}, any> {
     render() {
         return <div className="container App-content">
             <div className="row">
-                <div className="col pt-5">
-                    <p>Hi there!</p>
+                <div className="col pt-5 .offset-md-5">
+                    <p>Hi there - why are you here? I probably sent this to you to show off some of my random coding I've been doing :) Anyway - I'm Matt, and I like to code</p>
                 </div>
             </div>
                 <ul className="list-inline fixed-bottom">
